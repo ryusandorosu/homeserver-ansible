f5e2d019b02e025c137350424335d43637fa17a2
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-08 15:20:49 +0300

    figured out that nft has different format of timeout that bantime in f2b, passing it differently

--- roles/notifications/files/scripts/f2b-notifications.sh
+++ roles/notifications/files/scripts/f2b-notifications.sh
@@ -14,17 +14,13 @@ else
   if grep -m 20 -awF $banned_ip $logpath | grep -a "Restore Ban"; then exit 0; fi
 fi
 
-BANTIME=" $(date -d@$bantime -u +%_H | sed 's/\ //') hours"
-if (( $bantime >= 86400 )); then
-BANTIME=" $(date -d@$bantime -u +%_d) days"
-elif [[ $jail_name == recidive ]]; then
-BANTIME="ever"
-fi
+[[ $jail_name == recidive ]] && bantime="ever"
 
-MESSAGE="[Fail2Ban] <b>$jail_name</b>: banned <code>$banned_ip</code> from $(hostname) for<b>$BANTIME</b>
+MESSAGE="[Fail2Ban] <b>$jail_name</b>: banned <code>$banned_ip</code> from $(hostname) for <b>$bantime</b>
 failures: $failures | ipfailures: $ipfailures | ipjailfailures: $ipjailfailures
 <pre>$(curl -s http://ip-api.com/json/$banned_ip | jq)</pre>
 logpath: <code>$logpath</code>
-<pre>$(grep -m 20 -awF $banned_ip $logpath | grep -v ' sudo: ' | tail -n 20)</pre>"
+<pre>$(grep -m 20 -awF $banned_ip $logpath | grep -v ' sudo: ' | tail -n 20)</pre>
+"
 
 /usr/local/bin/tgbot_notify.sh "$MESSAGE"
