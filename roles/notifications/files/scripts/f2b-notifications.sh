5d79b408e5c0a3f80e48fd6e65e56f66707b9575
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-10 14:27:07 +0300

    added regex on single/plural numbers of bantime

--- roles/notifications/files/scripts/f2b-notifications.sh
+++ roles/notifications/files/scripts/f2b-notifications.sh
@@ -8,18 +8,19 @@ failures="$5"
 ipfailures="$6"
 ipjailfailures="$7"
 
-if grep -m 20 -awF $banned_ip /var/log/fail2ban.log | grep -aE "Restore Ban|already banned"; then exit 0; fi
+loglines=20
+if grep -m $loglines -awF $banned_ip /var/log/fail2ban.log | grep -aE "Restore Ban|already banned"; then exit 0; fi
 
 BANTIME=" $(date -d@$bantime -u +%_H | sed 's/\ //') hours"
 (( $bantime >= 86400 )) && BANTIME=" $(date -d@$bantime -u +%_d) days"
 (( $bantime >= 2678400 )) && BANTIME=" $(date -d@$bantime -u +%_d) months"
-#[[ "$BANTIME" == "" ]] #need regex here to detect plural if there is 1
+[[ "$BANTIME" =~ ^\ 1\ (hours|days|months)$ ]] && BANTIME=${BANTIME%s}
 (( $bantime == -1 )) && BANTIME="ever"
 
-MESSAGE="[Fail2Ban] <b>$jail_name</b>: banned <code>$banned_ip</code> from $(hostname) for<b>$BANTIME</b>
+MESSAGE="[Fail2Ban] <b>$jail_name</b>: banned <code>$banned_ip</code> from $(hostname) for<b>$BANTIME</b> (debug: $bantime)
 failures: $failures | ipfailures: $ipfailures | ipjailfailures: $ipjailfailures
 <pre>$(get_ip_geo $banned_ip | jq)</pre>
 logpath: <code>$logpath</code>
-<pre>$(grep -m 20 -awF $banned_ip $logpath | grep -v ' sudo: ' | tail -n 20)</pre>"
+<pre>$(grep -m $loglines -awF $banned_ip $logpath | grep -v ' sudo: ' | tail -n $loglines)</pre>"
 
 /usr/local/bin/tgbot_notify.sh "$MESSAGE"
