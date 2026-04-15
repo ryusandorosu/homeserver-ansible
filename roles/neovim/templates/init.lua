79cffdcbe9da8ba87b63d1c33b70cc9d63634da5
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-04-15 20:22:58 +0300

    solved clipboard problem with nvim

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -3,8 +3,8 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.clipboard = "unnamedplus"
 vim.g.mapleader = " "
+require("config.clipboard")
 require("config.lazy")
 require("config.lsp")
 require("config.cmp")
---
+++ roles/neovim/templates/lua/config/clipboard.lua
@@ -0,0 +1,13 @@
+vim.g.clipboard = {
+  name = 'osc52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
+
+vim.opt.clipboard = "unnamedplus"
