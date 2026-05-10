44f17b2feb1c7668a1e53fd7ae0ae52cb5be2f1c
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-10 10:25:38 +0300

    refactored grafana alerting notification message template, but values are still empty

--- roles/grafana/templates/alerting/telegram.go
+++ roles/grafana/templates/alerting/telegram.go
@@ -1,10 +1,28 @@
 {{ range .Alerts }}
-{{ if eq .Status "firing" }}🔴 {{ else if eq .Status "resolved" }}🟢 {{ end }}nuc-server grafana alert: {{ .Status }}: {{ if eq .Labels.severity "error" }}🔴 {{ else if eq .Labels.severity "warning" }}🟡 {{ end }}{{ with .Labels.severity }}{{ . }}{{ else }}n/a{{ end }}
+{{- if eq .Status "firing" -}}🔴{{ " " }}
+{{- else if eq .Status "resolved" -}}🟢{{ " " }}
+{{- end -}}
+nuc-server grafana alert: {{ .Status }}:{{ " " }}
+{{- if eq .Labels.severity "error" -}}🔴{{ " " }}
+{{- else if eq .Labels.severity "warning" -}}🟡{{ " " }}
+{{- end -}}
+{{ with .Labels.severity -}}{{ . }}{{ else -}}n/a{{ end }}
 Alert: <code>{{ with .Annotations.alertname }}{{ . }}{{ else with .Labels.alertname }}{{ . }}{{ else }}n/a{{ end }}</code>
-Value: <code>{{ with .Values }}{{range $$refID, $$value := .Values}}
-{{$$refID}} = {{$$value}}{{end}}{{ else }}n/a{{ end }}</code>
+Value: <code>
+{{- if .ValueString -}}
+{{ reReplaceAll "(?s).*var='[^']*_value'.*?value=([0-9.]+).*" "$1" .ValueString }}
+{{- else -}}
+n/a
+{{- end -}}
+</code>
 Threshold: <code>{{ with .Annotations.threshold }}{{ . }}{{ else }}n/a{{ end }}</code>
 Description:
 <i>{{ with .Annotations.description }}{{ . }}{{ else }}n/a{{ end }}</i>
-{{if gt (len .GeneratorURL) 0}}<a href="{{.GeneratorURL}}">Alert</a>{{end}}{{if gt (len .PanelURL) 0}} | {{end}}{{if gt (len .PanelURL) 0}}<a href="{{.PanelURL}}">Dashboard panel</a>{{end}}
+{{ if gt (len .GeneratorURL) 0 -}}
+<a href="{{.GeneratorURL}}">Alert</a>
+{{- end -}}
+{{ if gt (len .PanelURL) 0 }} | {{ end }}
+{{- if gt (len .PanelURL) 0 -}}
+<a href="{{.PanelURL}}">Dashboard panel</a>
+{{- end -}}
 {{ end }}
