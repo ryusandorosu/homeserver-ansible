964c3e9cc7c8ebacf5a4d0e613d0bfb174e99746
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-11 22:21:13 +0300

    well finally solved grafana notifications grouping issue?

--- roles/grafana/templates/alerting/telegram.go
+++ roles/grafana/templates/alerting/telegram.go
@@ -7,7 +7,7 @@ nuc-server grafana alert: {{ .Status }}:{{ " " }}
 {{- else if eq .Labels.severity "warning" -}}🟡{{ " " }}
 {{- end -}}
 {{ with .Labels.severity -}}{{ . }}{{ else -}}n/a{{ end }}
-Alert: <code>{{ with .Annotations.alertname }}{{ . }}{{ else with .Labels.alertname }}{{ . }}{{ else }}n/a{{ end }}</code>
+Alert: <code>{{ with .Labels.rulename }}{{ . }}{{ else with .Labels.alertname }}{{ . }}{{ else }}n/a{{ end }}</code>
 Value: <code>
 {{- if .Values -}}
 {{- range $$k, $$v := .Values -}}
@@ -28,5 +28,6 @@ Description:
 {{ if gt (len .PanelURL) 0 }} | {{ end }}
 {{- if gt (len .PanelURL) 0 -}}
 <a href="{{.PanelURL}}">Dashboard panel</a>
-{{- end -}}
+{{- end }}
+
 {{ end }}
