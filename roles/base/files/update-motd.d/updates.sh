45306754b6905c31552873d48086cf1166a7d8b9
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-14 17:29:18 +0300

    added printing list of upgradable packets to motd

--- roles/base/files/update-motd.d/updates.sh
+++ roles/base/files/update-motd.d/updates.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-UPDATES=$(apt list --upgradable 2>/dev/null | grep -c upgradable)
+UPDATES="$(apt list --upgradable 2>/dev/null | tail -n +2)"
+COUNT=$(printf "%s\n" "$UPDATES" | grep -c .)
 
-if [ "$UPDATES" -gt 0 ]; then
-  echo "🔴 $UPDATES updates available"
+if [ "$COUNT" -gt 0 ]; then
+  echo "🔴 $COUNT updates available:"
+  printf "%s\n" "$UPDATES"
 else
   echo "🟢 System is up to date"
 fi
