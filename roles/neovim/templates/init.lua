dc47e95b5b06de05f63cafb9871c1dfbc9a12e51
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-05 18:56:29 +0300

    adjusted cursor width in all modes and disabled relative lines number setting

--- roles/neovim/templates/init.lua
+++ roles/neovim/templates/init.lua
@@ -1,5 +1,5 @@
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
@@ -12,3 +12,4 @@ require("config.cmp")
 vim.cmd.colorscheme("gruvbox")
 vim.opt.termguicolors = true
 require("bufferline").setup{}
+vim.opt.guicursor = "i-c-v-n:block"
