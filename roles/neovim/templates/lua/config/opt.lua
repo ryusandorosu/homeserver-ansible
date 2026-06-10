27261903e6e05da762568e77337af3855e8842e7
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-10 23:50:14 +0300

    configured more nvim opts and visually separated them

--- roles/neovim/templates/lua/config/opt.lua
+++ roles/neovim/templates/lua/config/opt.lua
@@ -1,8 +1,21 @@
+vim.opt.termguicolors = true
+vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
+
 vim.opt.number = true
 vim.opt.relativenumber = false
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.opt.termguicolors = true
+vim.opt.cursorline = true
 vim.opt.guicursor = "i-c-v-n:block"
-vim.opt.guifont = "JetBrainsMono Nerd Font:h12"
+
+-- indentation/tabbing settings
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true -- при переносе строки добавлять отступы
+
+-- enable undo after file closing
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+vim.opt.shada = "!,'1000,<50,s10,h"
+vim.opt.confirm = true
