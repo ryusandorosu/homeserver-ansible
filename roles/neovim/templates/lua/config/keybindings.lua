acc73ce04b0ad0ea98672f5494a70ded84eda8c0
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-10 22:58:33 +0300

    moved nvim plugins keymaps which were possible to keybindings config

--- roles/neovim/templates/lua/config/keybindings.lua
+++ roles/neovim/templates/lua/config/keybindings.lua
@@ -1,4 +1,25 @@
+-- :help default-mappings
+-- :h nvim-surround.configuration -- default is 'S' when selected in visual mode
+-- :h comment.config -- default is 'gc'(nv) - opleader and 'gcc'(n) - toggler for selected lines. redefined to 'c' and 'cc'
+-- ctrl-q and button: key
 vim.g.mapleader = " "
+vim.keymap.set('n', '<C-w>', ':w<CR>', { desc = "Save the file" })
+vim.keymap.set('n', '<C-x>', ':q<CR>', { desc = "Close current window" })
+vim.keymap.set({'n','v'}, '<C-a>', 'ggVG', { desc = "Select all text" })
+vim.keymap.set('i', '<C-a>', '<Esc>ggVG', { desc = "Select all text" })
+vim.keymap.set('n', '<leader>h', ':split<CR>', { desc = "Horizontal split" })
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>', { desc = "Vertical split" })
+
+-- neotree
+vim.keymap.set('n', '<C-n>', ':Neotree <CR>')
+vim.keymap.set('n', '<C-Right>', ':bnext<CR>') --<leader><Right>
+vim.keymap.set('n', '<C-Left>', ':bprevious<CR>') --<leader><Left>
+
+-- telescope
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fw', ':Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<cr>')
+
 -- line moving
 vim.keymap.set('n', '<C-Up>', ':m .-2<CR>==', { desc = "Move line up" })
 vim.keymap.set('n', '<C-Down>', ':m .+1<CR>==', { desc = "Move line down" })
@@ -14,27 +35,16 @@ vim.keymap.set('v', '<Tab>', '>gv')
 vim.keymap.set('v', '<S-Tab>', '<gv')
 
 -- line duplication
--- NORMAL
-vim.keymap.set('n', '<C-d>', function()
+vim.keymap.set({'n','v'}, '<C-d>', function()
   local line = vim.api.nvim_get_current_line()
   vim.api.nvim_put({line}, 'l', true, false)
 end, { desc = "Duplicate line" })
 
--- INSERT
-vim.keymap.set('i', '<C-d>', function()
-  local line = vim.api.nvim_get_current_line()
-  vim.api.nvim_put({line}, 'l', true, false)
-end, { desc = "Duplicate line" })
-
--- VISUAL
 vim.keymap.set('v', '<leader>d', function()
   local start_line = vim.fn.line("'<")
   local end_line = vim.fn.line("'>")
-
   local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
-
   vim.api.nvim_buf_set_lines(0, end_line, end_line, false, lines)
-
   vim.api.nvim_win_set_cursor(0, { end_line + 1, 0 })
   vim.cmd("normal! V" .. (#lines - 1) .. "j")
 end, { desc = "Duplicate selection" })
--- roles/neovim/templates/lua/plugins/comment.lua
+++ roles/neovim/templates/lua/plugins/comment.lua
@@ -2,13 +2,14 @@ return {
   {
     "numToStr/Comment.nvim",
     config = function()
-      require("Comment").setup()
+      -- https://github.com/numToStr/Comment.nvim#setup
+      require("Comment").setup({
+        opleader = { line = 'c' }, --normal, visual
+        toggler = { line = 'cc' }, --normal
+      })
       local api = require("Comment.api")
       vim.keymap.set({'i', 'n'}, '<leader>/', function()
         api.toggle.linewise.current()
-      end)
-      vim.keymap.set('v', '<leader>/', function()
-        api.toggle.linewise(vim.fn.visualmode())
       end)
     end
   }
