b610f3277dd9fb97d63cdd2b76631539728d3063
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-10 23:48:30 +0300

    moved non-configured nvim plugins to common lua file

--- roles/neovim/templates/lua/plugins.lua
+++ roles/neovim/templates/lua/plugins.lua
@@ -1,11 +1,28 @@
 return {
 
-  { "nvim-lualine/lualine.nvim",
+  {
+    "nvim-lualine/lualine.nvim",
     config = function()
       require("lualine").setup()
     end
   },
-  
+
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('telescope').setup({})
+    end
+  },
+
   { "tpope/vim-fugitive" },
   { "airblade/vim-gitgutter" },
 
--- roles/neovim/templates/lua/plugins/surround.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    config = function()
-      require("nvim-surround").setup({})
-    end
-  }
-}
