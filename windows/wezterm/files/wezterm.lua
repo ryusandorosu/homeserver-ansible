96fdf3f88fb5bd30e83c9451968f725a6a0af8f7
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-18 20:07:54 +0300

    tried all selected wezterm themes and filtered most of them

--- windows/wezterm/files/wezterm.lua
+++ windows/wezterm/files/wezterm.lua
@@ -9,22 +9,12 @@ config.initial_rows = 28
 config.font_size = 10
 config.font = wezterm.font 'JetBrains Mono'
 config.default_prog = { 'ssh', 'nuc' }
--- most preferred themes
-config.color_scheme = 'Tango (terminal.sexy)'
 -- config.color_scheme = 'Orangish (terminal.sexy)'
--- config.color_scheme = 'Muse (terminal.sexy)'
--- also ok themes
--- config.color_scheme = 'Red Alert'
--- config.color_scheme = 'Red Sands'
 -- config.color_scheme = 'Konsolas'
--- config.color_scheme = 'Iiamblack (terminal.sexy)'
--- config.color_scheme = 'Digerati (terminal.sexy)'
+config.color_scheme = 'Seti'
 -- config.color_scheme = 'Hurtado'
--- config.color_scheme = 'Ibm3270 (Gogh)'
 -- config.color_scheme = 'NightLion v1'
--- config.color_scheme = 'Numix Darkest (terminal.sexy)'
--- config.color_scheme = 'Seti'
--- config.color_scheme = 'Sundried'
+-- config.color_scheme = 'Red Alert'
 
 -- Finally, return the configuration to wezterm:
 return config
