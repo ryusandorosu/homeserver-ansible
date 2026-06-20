4fd9d492d5988bfc7432bed1577cfad4a53eeda9
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-21 00:14:36 +0300

    composed ssh role for windows and modified win hostname conditions, removed unnecessary device references

--- tools/j2render.sh
+++ tools/j2render.sh
@@ -7,8 +7,10 @@ template="$1"
 
 repo_root="$(git rev-parse --show-toplevel)"
 relative_template="$(realpath --relative-to="$repo_root" "$template")"
-roles_root="$(awk -F/ -v r=$repo_root '/r/ {print $1}' <<< "$relative_template")"
-role="$(awk -F/ -v r=$roles_root '/r/ {print $2}' <<< "$relative_template")"
+# roles_root="$(awk -F/ -v r=$repo_root '/r/ {print $1}' <<< "$relative_template")"
+# role="$(awk -F/ -v r=$roles_root '/r/ {print $2}' <<< "$relative_template")"
+roles_root="$(echo "$relative_template" | cut -d/ -f1)"
+role="$(echo "$relative_template" | cut -d/ -f2)"
 outfile="$repo_root/${relative_template%.j2}"
 
 ANSIBLE_BECOME_ASK_PASS=False \
