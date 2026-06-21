0d02c3bb651cc7f259f4c0a8c4e2983b2e4039e3
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-22 00:05:44 +0300

    customized wezterm by adding barline plugins and set ability to configure modules

---
+++ windows/wezterm/files/bar.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+local config = {}
+bar.apply_to_config(config, {
+  modules = {
+    tabs = {
+      inactive_tab_fg = 8,
+      new_tab_fg = 8,
+    },
+    workspace = { enabled = false },
+    ssh = { enabled = true },
+    username = { color = 8 },
+    clock = { format = "%H:%M:%S" },
+  },
+})
+return bar
+-- does not have effect on bar plugin:
+--config.tab_bar_at_bottom = true,
+--config.use_fancy_tab_bar = false,
---
+++ windows/wezterm/files/tabline.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+  options = {
+    theme = 'Abernathy',
+  },
+-- config.color_scheme = 'Abernathy'
+-- config.color_scheme = 'Adventure'
+  sections = {
+    tabline_a = { 'hostname' }, --mode
+    tabline_b = { 'domain' }, --workspace
+    tabline_x = { {'ram', use_pwsh = true}, {'cpu', use_pwsh = true} },
+    tabline_y = {}, -- 'hostname', 'domain'
+    tabline_z = { 'battery', {'datetime', style = '%H:%M:%S'} },
+  },
+})
+--tabline.apply_to_config(config)
+return tabline
+--does not work with tab_bar_at_bottom = true
