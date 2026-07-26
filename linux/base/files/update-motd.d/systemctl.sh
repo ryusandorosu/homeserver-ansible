a725697dee027747a09142da838c7b22c2cb66fc
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-27 00:10:11 +0300

    managed to make highlighting work on motd printing

--- linux/base/files/update-motd.d/systemctl.sh
+++ linux/base/files/update-motd.d/systemctl.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-systemctl --state=failed
+red='\033[91m'
+nocolor='\033[0m'
+
+print_highlighted() {
+  local text="$1"
+  local highlight="$2"
+  local color="$3"
+  echo "$text" | sed -E "s/${highlight}/$(printf $color)${highlight}$(printf $nocolor)/"
+}
+
+print_highlighted "$(systemctl --state=failed --quiet)" ● "${red}"
