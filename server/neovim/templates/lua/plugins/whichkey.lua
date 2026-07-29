93ac5e5aa024082def3e4523e1817126b5ea23a1
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-29 22:22:32 +0300

    disabled whichkeys leader? keymapping

--- server/neovim/templates/lua/plugins/whichkey.lua
+++ server/neovim/templates/lua/plugins/whichkey.lua
@@ -4,13 +4,13 @@ return {
     event = "VeryLazy",
     -- https://github.com/folke/which-key.nvim#-triggers
     opts = {},
-    keys = {
-      {
-        "<leader>?", function()
-          require("which-key").show({ global = false })
-        end, 
-        desc = "Buffer Local Keymaps (which-key)"
-      },
-    },
+    -- keys = {
+    --   {
+    --     "<leader>?", function()
+    --       require("which-key").show({ global = false })
+    --     end, 
+    --     desc = "Buffer Local Keymaps (which-key)"
+    --   },
+    -- },
   },
 }
