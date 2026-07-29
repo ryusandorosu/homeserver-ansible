28578a0677951a97d05c3a0ef691c758dbbca943
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-29 20:48:11 +0300

    included fzf native dependency for telescope plugin

--- server/neovim/templates/lua/plugins.lua
+++ server/neovim/templates/lua/plugins.lua
@@ -17,7 +17,10 @@ return {
 
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
       require('telescope').setup({})
     end
