3e74e64a8760c51276341c52ae818cce2c9670df
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-29 22:21:27 +0300

    discarded bufferline plugin and preferred cokeline instead

---
+++ server/neovim/templates/lua/plugins/cokeline.lua
@@ -0,0 +1,12 @@
+return {
+
+  {
+    "willothy/nvim-cokeline",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- If you want devicons
+      "stevearc/resession.nvim"       -- Optional, for persistent history
+    },
+    config = true
+  },
+
+}
