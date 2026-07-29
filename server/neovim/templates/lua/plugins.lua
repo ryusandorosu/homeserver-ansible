6163e2e780f5be09dffd1f134f79d3689c17b042
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-30 00:36:22 +0300

    separated telescope plugin lua from common plugins and configured its ripgrep and fd usage to exclude the same dirs as it has been done for zsh fzf walker

--- server/neovim/templates/lua/plugins.lua
+++ server/neovim/templates/lua/plugins.lua
@@ -15,19 +15,8 @@ return {
     end
   },
 
-  {
-    "nvim-telescope/telescope.nvim",
-    -- https://github.com/nvim-telescope/telescope.nvim#pickers
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    },
-    config = function()
-      require('telescope').setup({})
-    end
-  },
-
   { "tpope/vim-fugitive" },
   { "airblade/vim-gitgutter" },
+  -- https://nvimdev.github.io/lspsaga/
 
 }
