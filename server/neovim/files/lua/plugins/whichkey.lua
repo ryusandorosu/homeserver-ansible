0c265e0e0619b27420f25eda77c8c0bd29cdbe31
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-31 01:38:01 +0300

    tried hydra mode for whichkey neovim plugin but commented it too

--- server/neovim/files/lua/plugins/whichkey.lua
+++ server/neovim/files/lua/plugins/whichkey.lua
@@ -3,13 +3,14 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     -- https://github.com/folke/which-key.nvim#-triggers
+    -- https://www.lazyvim.org/plugins/editor#which-keynvim
     opts = {},
     -- keys = {
     --   {
     --     "<leader>?", function()
-    --       require("which-key").show({ global = false })
-    --     end, 
-    --     desc = "Buffer Local Keymaps (which-key)"
+    --       require("which-key").show({ keys = "<c-w>", loop = true })
+    --     end,
+    --     desc = "Window Hydra Mode (which-key)",
     --   },
     -- },
   },
