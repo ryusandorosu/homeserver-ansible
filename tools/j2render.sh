0a7823aaec126cea6ab93719a3c3aa9722f13c3a
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-08-01 11:43:47 +0300

    predefined most common cases for jinja loop variables substitution in vscode renderer tool

--- tools/j2render.sh
+++ tools/j2render.sh
@@ -11,11 +11,31 @@ roles_root="$(echo "$relative_template" | cut -d/ -f1)"
 role="$(echo "$relative_template" | cut -d/ -f2)"
 outfile="$repo_root/${relative_template%.j2}"
 
+# $2 is optional. defines jinja expression type for {{ item }}
+item_pattern="$2"
+item_arg="$3"
+item_expr=""
+case "$item_pattern" in
+  # eg.: "nginx_public_sites[0]"  -- works
+  list-index)
+    var="${item_arg%% *}"; idx="${item_arg#* }"
+    item_expr="${var}[${idx}]"
+    ;;
+  # eg.: "(nginx_private_sites | dict2items)[0]"
+  dict2items-index)
+    var="${item_arg%% *}"; idx="${item_arg#* }"
+    item_expr="(${var} | dict2items)[${idx}]"
+    ;;
+  # eg.: "(nginx_private_sites | dict2items | selectattr('key','equalto','guac') | first)"
+  dict2items-key)
+    var="${item_arg%% *}"; key="${item_arg#* }"
+    item_expr="(${var} | dict2items | selectattr('key','equalto','${key}') | first)"
+    ;;
+  none|"")
+    item_expr=""
+    ;;
+esac
 extra_item_arg=()
-item_expr="$2"
-# $2 is optional. jinja expression for {{ item }}
-# eg.: "(nginx_private_sites | dict2items)[0]"
-# eg.: "(nginx_private_sites | dict2items | selectattr('key','equalto','guac') | first)"
 [[ -n "$item_expr" ]] && extra_item_arg=(-e "item={{ $item_expr }}")
 
 ANSIBLE_BECOME_ASK_PASS=False \
