d6fea36b88fc90d93e3f05f84481a7f5692d557a
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-08-02 17:43:40 +0300

    turns out that i dont even need tabline plugin to show tabs, lualine already does it

--- server/neovim/files/lua/plugins/lualine.lua
+++ server/neovim/files/lua/plugins/lualine.lua
@@ -75,33 +75,6 @@ return {
   },
 
   {
-    "kdheepak/tabline.nvim",
-    dependencies = {
-      { "nvim-lualine/lualine.nvim", opt = true },
-      { "nvim-tree/nvim-web-devicons", opt = true },
-    },
-    -- https://github.com/kdheepak/tabline.nvim#installation
-    config = function()
-      require("tabline").setup {
-        enable = true,
-        options = {
-          max_bufferline_percent = 66,
-          show_tabs_always = false,
-          show_devicons = true,
-          show_bufnr = false,
-          show_filename_only = true,
-          modified_italic = false,
-          show_tabs_only = false,
-        }
-      }
-      vim.cmd[[
-        set guioptions-=e " Use showtabline in gui vim
-        set sessionoptions+=tabpages,globals " store tabpages and globals in session
-      ]]
-    end,
-  },
-
-  {
     "willothy/nvim-cokeline",
     enabled = false,
     dependencies = {
